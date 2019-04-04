58119dc23cfc68a307d6f47c8bccc994552d356c
lore893 <lore-maro-93@hotmail.it>
2019-04-04 16:24:41 +0200

    Cambiato py_convert.py
    Automatizzazione della creazione e della cancellazione del file GlobalJSON.json e dell'inizializzazione dei singoli file JSON

--- py_convert.py
+++ py_convert.py
@@ -1,6 +1,12 @@
 import json
 import os
 
+listaFileRepo = os.listdir()
+
+if("GlobalJSON.json" in listaFileRepo):
+    os.remove("GlobalJSON.json")
+
+
 def inizializzazioneJSON(nomeFile, var_js):
     with open(nomeFile, 'r') as f:
         contenuto = f.read()
@@ -9,16 +15,30 @@ def inizializzazioneJSON(nomeFile, var_js):
     with open(nomeFile, 'a') as f:
         f.write(contenuto)
 
+
 listaFile = os.listdir('../materiale_Nocentini/oeis-tools-master/src/fetched')
 
 index = 0
 inizializzazioneSequenze = []
 
+'''for j in listaFile:
+    if j.endswith(".json"):
+        with open(j, 'r') as f:
+            testo = f.read()
+            if testo[0] == 's':
+                scritto = True
+                break'''
+
+'''if scritto == False:'''
 for i in listaFile:
     if i.endswith(".json"):
         cosaScrivo = "sequenzaJSON" + str(index) + " = "
         inizializzazioneSequenze.append(cosaScrivo[0:-3])
-        inizializzazioneJSON('../materiale_Nocentini/oeis-tools-master/src/fetched/' + i, cosaScrivo)
+        with open('../materiale_Nocentini/oeis-tools-master/src/fetched/' + i, 'r') as f:
+            testo = f.read()
+        if (testo[0] != "s"):
+            inizializzazioneJSON(
+                '../materiale_Nocentini/oeis-tools-master/src/fetched/' + i, cosaScrivo)
         index = int(index) + 1
 
 JSONtotale = []
@@ -30,9 +50,11 @@ for i in listaFile:
 
 with open('GlobalJSON.json', 'a+') as f:
     for i in JSONtotale:
-       f.write(i + '\n')
+        f.write(i + '\n')
     f.write('json_list = [')
+    print(inizializzazioneSequenze)
     for j in inizializzazioneSequenze:
+
         if (j != inizializzazioneSequenze[len(inizializzazioneSequenze)-1]):
             f.write(j + ', ')
     f.write(inizializzazioneSequenze[len(inizializzazioneSequenze)-1])
