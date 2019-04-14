17ec4b1dfed88eb33669ba272f7228a14303c196
Marco Vignini <marco.vignini@stud.unifi.it>
2019-04-14 16:51:11 +0200

    Update fetchAndRebuild.php
    - added submit button with a image
    - edited fetchAndRebuild.php

--- fetchAndRebuild.php
+++ fetchAndRebuild.php
@@ -1,23 +1,56 @@
-<?php
+<html>
 
-$sequence = $_POST['sequence'];
+<head>
 
-#2>&1
+    <style>
+        body {
+            margin: 10;
+            background-color: #D3D3D3;
+        }
 
-$wd = getcwd();
+        #back {
+            width: 70px;
+            height: 50px;
+        }
+    </style>
 
-shell_exec("py py_utility.py");
+</head>
 
-chdir($_SERVER["DOCUMENT_ROOT"]);
+<body>
 
-chdir("materiale_Nocentini\oeis-tools-master\src");
+    <font face="Comics Sans MC" size=5>
+        The sequence has been fetched correctly! 
+    </font>
 
-shell_exec("py crawling.py ".$sequence);
+    <br>
+    <br>
 
-chdir($_SERVER["DOCUMENT_ROOT"]);
+    <form action="index.html" id="form">
+        <input type="image" src="backIcon.png" class="btTxt submit" id="back"/>
+    </form>
 
-chdir($wd);
+    <?php
 
-shell_exec("py py_convert.py");
+    $sequence = $_POST['sequence'];
 
-?> 
+    $wd = getcwd();
+
+    shell_exec("py py_utility.py");
+
+    chdir($_SERVER["DOCUMENT_ROOT"]);
+
+    chdir("materiale_Nocentini\oeis-tools-master\src");
+
+    shell_exec("py crawling.py " . $sequence);
+
+    chdir($_SERVER["DOCUMENT_ROOT"]);
+
+    chdir($wd);
+
+    shell_exec("py py_convert.py");
+
+    ?>
+
+    <body>
+
+</html>
