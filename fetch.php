c037f05af5d0c94163afff8ce16e444e6dc01c51
Marco Vignini <marco.vignini@stud.unifi.it>
2019-04-24 13:54:32 +0200

    files json management changed
    - Implemented ajax requests to localhost for reading the json files
    
    - small changes

--- fetch.php
+++ fetch.php
@@ -2,20 +2,10 @@
 
     $sequence = $_GET['download'];
 
-    $wd = getcwd();
-
-    shell_exec("py py_utility.py");
-
     chdir($_SERVER["DOCUMENT_ROOT"]);
 
     chdir("materiale_Nocentini\oeis-tools-master\src");
 
     shell_exec("py crawling.py " . $sequence);
-
-    chdir($_SERVER["DOCUMENT_ROOT"]);
-
-    chdir($wd);
-
-    shell_exec("py py_convert.py");
 
     ?>
