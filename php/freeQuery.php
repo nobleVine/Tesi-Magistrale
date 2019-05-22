e558921564c097dc9533670c2424662eee41aad8
Marco Vignini <marco.vignini@stud.unifi.it>
2019-05-22 18:03:37 +0200

    free queries
    - added a textArea in bigGraph.html
    - added script php for all queries

---
+++ php/freeQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+$textQuery = $_POST['string1'];
+
+require_once 'readAndConnection.php';
+
+function freeQuery($textQuery, $connectionToDB) {
+    return $connectionToDB->query($textQuery);
+}
+
+$query_result = freeQuery($textQuery, $connectionToDB);
+
+$r = array();
+
+while ($row = $query_result->fetch_assoc()) {
+    array_push($r, $row);
+}
+
+echo json_encode($r);
+
+?>
